72d3e37295b1e659ee433a4cc4d1069682c34937
goosst <goosst@gmail.com>
2019-08-02 16:30:26 +0200

    added temperature and humidity sensor SHT30

--- wemos/sketch/sketch.ino
+++ wemos/sketch/sketch.ino
@@ -1,22 +1,27 @@
 // sg 6 may 2019
 // started from: https://github.com/esp8266-examples/ota-mqtt/blob/master/ota-mqtt/ota-mqtt.ino
-// broadcasts an mqtt message corresponding the high or low state of the input (to the home assistant broker)
-// allows over the air software update through the arduino ide
+// edited to my home assistant framework using a wemos D1 R1
+// 2 aug 2019: added SHT30
 
 #include <ESP8266WiFi.h>  //For ESP8266
 #include <PubSubClient.h> //For MQTT
 #include <ESP8266mDNS.h>  //For OTA
 #include <WiFiUdp.h>      //For OTA
 #include <ArduinoOTA.h>   //For OTA
+#include <WEMOS_SHT3X.h> //For SHT30
 
 #include "configuration.h" // to store passwords of mqtt and wifi
 
+SHT3X sht30(0x45);
+float Temp;
+float Hum;
 
 String mqtt_client_id = "ESP8266-"; //This text is concatenated with ChipId to get unique client_id
 //MQTT Topic configuration
 String mqtt_base_topic = "/sensor/" + mqtt_client_id + "/data";
-//#define humidity_topic "/humidity"
-//#define temperature_topic "/temperature"
+#define humidity_topic "/humidity"
+#define temperature_topic "/temperature"
+#define alarm_topic "/alarm"
 
 //MQTT client
 WiFiClient espClient;
@@ -115,14 +120,17 @@ bool checkBound(float newValue, float prevValue, float maxDiff) {
 
 long now = 0; //in ms
 long lastMsg = 0;
+float temp = 0.0;
+float hum = 0.0;
 float diff = 1.0;
-int min_timeout = 5000; //in ms
+int min_timeout = 2000; //in ms
 
 void loop() {
 
   ArduinoOTA.handle();
 
-  if (!mqtt_client.connected()) {
+  if (!mqtt_client.connected())
+  {
     mqtt_reconnect();
   }
   mqtt_client.loop();
@@ -132,10 +140,38 @@ void loop() {
     lastMsg = now;
     now = millis();
     int alarm_state = digitalRead(pin_alarm);
-    //Serial.println(alarm_state);
-    //Serial.println(String(alarm_state).c_str());
+    Serial.println(alarm_state);
 
-    mqtt_client.publish("sensor/alarm", String(alarm_state).c_str(), true);
+    //    String alarm_state_str="";
+    //    alarm_state_str=alarm_state_str+alarm_state;
+    //    Serial.println(alarm_state_str);
+    Serial.print("alarm status: ");
+    Serial.println(String(alarm_state).c_str());
+
+    //    mqtt_client.publish("sensor/alarm", String(alarm_state).c_str(), true);
+    mqtt_client.publish((mqtt_base_topic + alarm_topic).c_str(), String(alarm_state).c_str(), true);
+
+    if (sht30.get() == 0) {
+      Temp = sht30.cTemp;
+      Hum = sht30.humidity;
+
+      Serial.print("Temperature in Celsius : ");
+      Serial.println(Temp);
+      Serial.print("Relative Humidity : ");
+      Serial.println(Hum);
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("Error!");
+      Temp = 99999;
+      Hum = 99999;
+    }
+
+    mqtt_client.publish((mqtt_base_topic + temperature_topic).c_str(), String(Temp).c_str(), true);
+    mqtt_client.publish((mqtt_base_topic + humidity_topic).c_str(), String(Hum).c_str(), true);
 
   }
+
+
 }
