ff9b1babc9eea6199554c033a16bd79635d40eaa
goosst <goosst@gmail.com>
2021-01-17 21:32:07 +0100

    updating parameters using rest api octoprint

--- python_scripts/octoprint.py
+++ python_scripts/octoprint.py
@@ -2,9 +2,10 @@
 #  See documentation REST api of octoprint https://docs.octoprint.org/en/master/api/printer.html
 from requests import get,post
 import json
-import datetime
 import getopt
 import sys
+import datetime
+# from datetime import datetime, timedelta
 
 
 def update_hass_variables_seconds( address_hass,headers_hass,number_string,hass_variable ):
@@ -46,7 +47,6 @@ for opt, value in options:
 import headerfiles as parameters
 headers_octo=parameters.headers_octoprint
 address_octo=headers_octo['Host']
-
 
 headers_hass=parameters.headers
 address_hass=parameters.address_hass
@@ -112,3 +112,55 @@ payload = json.dumps(data)
 url='http://'+address_octo+':5000/api/printer/command'
 response = post(url,data=payload,headers=headers_octo)
 temp=response.text
+
+
+# stop printing
+entity_id='input_boolean.print_cancel'
+url='http://'+address_hass+':8123/api/states/'+entity_id
+response = get(url, headers=headers_hass)
+temp=response.text
+cancelrequest=json.loads(temp)
+
+data = {}
+if cancelrequest['state']=='on':
+    data['command']='cancel'
+    payload = json.dumps(data)
+    url='http://'+address_octo+':5000/api/job'
+    response = post(url,data=payload,headers=headers_octo)
+    temp=response.text
+
+    data['command'] = 'G28'
+    payload = json.dumps(data)
+    url='http://'+address_octo+':5000/api/printer/command'
+    response = post(url,data=payload,headers=headers_octo)
+    temp=response.text
+
+
+entity_id='input_boolean.print_pause_cyclic'
+url='http://'+address_hass+':8123/api/states/'+entity_id
+response = get(url, headers=headers_hass)
+temp=response.text
+periodicpause=json.loads(temp)
+
+if periodicpause['state']=='on':
+    url='http://'+address_hass+':8123/api/states/sensor.date_time_iso'
+    response = get(url, headers=headers_hass)
+    temp=response.text
+    readable_json=json.loads(temp)
+    
+    hass_time_real = datetime.datetime.strptime(readable_json['state'], '%Y-%m-%dT%H:%M:%S')
+    # pause every hour for 10 minutes
+    if hass_time_real.minute<10:
+        data['command']='pause'
+        data['action']='pause'
+        payload = json.dumps(data)
+        url='http://'+address_octo+':5000/api/job'
+        response = post(url,data=payload,headers=headers_octo)
+        temp=response.text
+    else:
+        data['command']='pause'
+        data['action']='resume'
+        payload = json.dumps(data)
+        url='http://'+address_octo+':5000/api/job'
+        response = post(url,data=payload,headers=headers_octo)
+        temp=response.text
