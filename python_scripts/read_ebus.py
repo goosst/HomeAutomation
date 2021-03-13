b57868d98bc55dcabd0aec659fb2587ae2f0fb7f
goosst <goosst@gmail.com>
2021-03-13 19:46:53 +0100

    modified heater system to new layout VR70 with VRC700 (and VR91f)

--- python_scripts/read_ebus.py
+++ python_scripts/read_ebus.py
@@ -3,6 +3,7 @@ import time
 from ilock import ILock
 import logging
 import os
+# 12 mar 21: adapted to new VR70 with VRC700 (and VR91f)
 
 numberOfIterationsMax=5
 cwd = os.getcwd()
@@ -18,36 +19,36 @@ logging.basicConfig(filename='debug_ebus',
 
 logging.debug("start directory")
 logging.debug(cwd)
-# with ILock('ebus', timeout=200):
-#read temperature measured by thermostat
-#time.sleep(3)
-logging.debug("read ebus started")
-cp = subprocess.run(["ebusctl read RoomTemp"],shell=True,stdout=subprocess.PIPE)
-logging.debug("read RoomTemp 1")
-logging.debug(cp)
-cp_string=cp.stdout.decode('utf-8')
-busread=cp_string[0:5]
-if busread == 'error':
-	# cp = subprocess.run(["ebusd -f --scanconfig >/dev/null 2>&1"],shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-	subprocess.Popen(["nohup", "ebusd", "-f", "--scanconfig"])
-	print('fubar')
-	time.sleep(60)
-	cp = subprocess.run(["ebusctl read RoomTemp"],shell=True,stdout=subprocess.PIPE)
-	logging.error("read RoomTemp 2")
-	logging.error(cp)
+with ILock('ebus', timeout=200):
+	#read temperature measured by thermostat
+	#time.sleep(3)
+	logging.debug("read ebus started")
+	cp = subprocess.run(["ebusctl read z1RoomTemp"],shell=True,stdout=subprocess.PIPE)
+	logging.debug("read RoomTemp 1")
+	logging.debug(cp)
 	cp_string=cp.stdout.decode('utf-8')
 	busread=cp_string[0:5]
-time.sleep(1)
-cntr=0
-while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
-	cp = subprocess.run(["ebusctl read RoomTemp"],shell=True,stdout=subprocess.PIPE)
-	cp_string=cp.stdout.decode('utf-8')
-	busread=cp_string[0:5]
-	time.sleep(5)
-	logging.error("counter:")
-	logging.error(cntr)
-	logging.error(busread)
-	cntr=cntr+1
+	if busread == 'error':
+		# cp = subprocess.run(["ebusd -f --scanconfig >/dev/null 2>&1"],shell=True,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		subprocess.Popen(["nohup", "ebusd", "-f", "--scanconfig"])
+		print('fubar')
+		time.sleep(60)
+		cp = subprocess.run(["ebusctl read z1RoomTemp"],shell=True,stdout=subprocess.PIPE)
+		logging.error("read RoomTemp 2")
+		logging.error(cp)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+	time.sleep(1)
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read z1RoomTemp"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.error("counter:")
+		logging.error(cntr)
+		logging.error(busread)
+		cntr=cntr+1
 msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature -u stijn -P mqtt -m "
 cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
 logging.debug("send mqtt RoomTemp")
@@ -59,61 +60,74 @@ time.sleep(3)
 
 
 # read temperature setpoint
-logging.debug("read DisplayedHc1RoomTempDesired")
-cp = subprocess.run(["ebusctl read DisplayedHc1RoomTempDesired"],shell=True,stdout=subprocess.PIPE)
-cp_string=cp.stdout.decode('utf-8')
-#print(cp_string)
-busread=cp_string[0:4]
-#print(busread)
-# if busread == 'erro':
-# 	#try again
-# 	logging.error("DisplayedHc1RoomTempDesired could not be read")
-# 	time.sleep(2)
-# 	cp = subprocess.run(["ebusctl read DisplayedHc1RoomTempDesired"],shell=True,stdout=subprocess.PIPE)
-# 	cp_string=cp.stdout.decode('utf-8')
-# 	busread=cp_string[0:4]
-cntr=0
-while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
-	cp = subprocess.run(["ebusctl read DisplayedHc1RoomTempDesired"],shell=True,stdout=subprocess.PIPE)
+logging.debug("read z1ActualRoomTempDesired")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read z1ActualRoomTempDesired"],shell=True,stdout=subprocess.PIPE)
 	cp_string=cp.stdout.decode('utf-8')
-	busread=cp_string[0:5]
-	time.sleep(5)
-	logging.debug("counter:")
-	logging.debug(cntr)
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read z1ActualRoomTempDesired"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_set -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt z1ActualRoomTempDesired")
 	logging.debug(busread)
-	cntr=cntr+1
-msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_set -u stijn -P mqtt -m "
-cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
-logging.debug("send mqtt DisplayedHc1RoomTempDesired")
-logging.debug(busread)
-logging.debug(cp)
-time.sleep(3)
+	logging.debug(cp)
+	time.sleep(3)
 
 # read temperature flow heating
-cp = subprocess.run(["ebusctl read Hc1ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
-cp_string=cp.stdout.decode('utf-8')
-#print(cp_string)
-busread=cp_string[0:4]
-# if busread == 'erro':
-# 	#try again
-# 	logging.error("Hc1ActualFlowTempDesired could not be read")
-# 	time.sleep(2)
-# 	cp = subprocess.run(["ebusctl read Hc1ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
-# 	cp_string=cp.stdout.decode('utf-8')
-# 	busread=cp_string[0:4]
-#print(busread)
-cntr=0
-while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+with ILock('ebus', timeout=200):
 	cp = subprocess.run(["ebusctl read Hc1ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
 	cp_string=cp.stdout.decode('utf-8')
-	busread=cp_string[0:5]
-	time.sleep(5)
-	logging.debug("counter:")
-	logging.debug(cntr)
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read Hc1ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_flowtemp -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+
+
+# read actual temperature flow
+logging.debug("read Hc1FlowTemp")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read Hc1FlowTemp"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read Hc1FlowTemp"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_flow -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt Hc1FlowTemp")
 	logging.debug(busread)
-	cntr=cntr+1
-msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_flowtemp -u stijn -P mqtt -m "
-cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug(cp)
+	time.sleep(3)
+
+
 
 # read time
 #cp = subprocess.run(["ebusctl read Time"],shell=True,stdout=subprocess.PIPE)
@@ -122,3 +136,156 @@ cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
 #msg1="mosquitto_pub -h localhost -t sensor/thermostat/fubar -u stijn -P mqtt -m "
 #print(time_read)
 #cp = subprocess.run([msg1+time_read],shell=True,stdout=subprocess.PIPE)
+
+##############################################################################
+###############################################################################
+# zolder
+
+# read temperature setpoint zolder
+logging.debug("read z2ActualRoomTempDesired")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read z2ActualRoomTempDesired"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read z2ActualRoomTempDesired"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_zolder_set -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt z2ActualRoomTempDesired")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
+
+
+
+# read temperature zolder
+logging.debug("read z2RoomTemp")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read z2RoomTemp"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read z2RoomTemp"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_zolder_vaillant -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt z2RoomTemp")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
+
+# read desired flow temperature zolder
+logging.debug("read Hc2ActualFlowTempDesired")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read Hc2ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read Hc2ActualFlowTempDesired"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_flowtemp_zolder_set -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt Hc2ActualFlowTempDesired")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
+
+# read actual temperature flow
+logging.debug("read Hc2FlowTemp")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read Hc2FlowTemp"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read Hc2FlowTemp"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_flowtemp_zolder -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt Hc2FlowTemp")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
+
+#######################################################################
+
+
+# read outside temperature
+logging.debug("read DisplayedOutsideTemp")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read DisplayedOutsideTemp"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read DisplayedOutsideTemp"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/temperature_outside_vaillant -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt DisplayedOutsideTemp")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
+
+# read waterpressure
+logging.debug("read WaterPressure")
+with ILock('ebus', timeout=200):
+	cp = subprocess.run(["ebusctl read WaterPressure"],shell=True,stdout=subprocess.PIPE)
+	cp_string=cp.stdout.decode('utf-8')
+	#print(cp_string)
+	busread=cp_string[0:4]
+	cntr=0
+	while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
+		cp = subprocess.run(["ebusctl read WaterPressure"],shell=True,stdout=subprocess.PIPE)
+		cp_string=cp.stdout.decode('utf-8')
+		busread=cp_string[0:5]
+		time.sleep(5)
+		logging.debug("counter:")
+		logging.debug(cntr)
+		logging.debug(busread)
+		cntr=cntr+1
+	msg1="mosquitto_pub -h localhost -t sensor/thermostat/pressure_vaillant -u stijn -P mqtt -m "
+	cp = subprocess.run([msg1+busread],shell=True,stdout=subprocess.PIPE)
+	logging.debug("send mqtt WaterPressure")
+	logging.debug(busread)
+	logging.debug(cp)
+	time.sleep(3)
--- python_scripts/set_temp_zolder.py
+++ python_scripts/set_temp_zolder.py
@@ -3,34 +3,48 @@ import time
 import getopt
 import sys
 from ilock import ILock
+import logging
+import os
+
+cwd = os.getcwd()
+path="/home/homeassistant/.homeassistant/www"
+os.chdir(path)
+
+logging.basicConfig(filename='debug_set_temp_zolder',
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.DEBUG)
+
+logging.debug("start directory")
+logging.debug(cwd)
 
 # check if passed options are valid
 try:
     options, args = getopt.getopt(sys.argv[1:], 't:',['temperature_setpoint='])
-    print(options)
-    print(args)
+    logging.debug(options)
+    logging.debug(args)
 except getopt.GetoptError:
-    print("incorrect syntax")
-    print("usage: python3 set_temperature.py -t <value>")
-    print("default to 12 degrees")
-    msg2=12
+    logging.debug("incorrect syntax")
+    logging.debug("usage: python3 set_temperature.py -t <value>")
+    logging.debug("default to 7 degrees")
+    msg2=7
     sys.exit(2)
 for opt, value in options:
     if opt in ('-t','-T','--temperature_setpoint'):
         msg2=value
-        print("successful argument")
-        print(msg2)
+        logging.debug("successful argument")
+        logging.debug(msg2)
 
 numberOfIterationsMax=5
 
-# msg1="ebusctl write -c f37 Hc1DayTemp "
 with ILock('ebus', timeout=200):
-    msg1="ebusctl write -c b7v z1DayTemp "
+    msg1="ebusctl write -c b7v z2DayTemp "
     cp = subprocess.run([msg1+msg2],shell=True,stdout=subprocess.PIPE)
 
-    msg1="ebusctl write -c b7v z1NightTemp "
+    msg1="ebusctl write -c b7v z2NightTemp "
     cp = subprocess.run([msg1+msg2],shell=True,stdout=subprocess.PIPE)
-    time.sleep(30)
+    time.sleep(15)
     correctSetting=False
     cntr1=0
     while (correctSetting == False) and (cntr1<numberOfIterationsMax):
@@ -38,25 +52,26 @@ with ILock('ebus', timeout=200):
         cntr=0
         busread='ERR'
         while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
-            cp = subprocess.run(["ebusctl read z1DayTemp"],shell=True,stdout=subprocess.PIPE)
+            cp = subprocess.run(["ebusctl read z2DayTemp"],shell=True,stdout=subprocess.PIPE)
             cp_string=cp.stdout.decode('utf-8')
             busread=cp_string[0:5]
-            print(busread)
+            logging.debug("z2daytemp")
+            logging.debug(busread)
             time.sleep(5)
             cntr=cntr+1
-            print(cntr)
+            logging.debug(cntr)
 
         #check if it is truly set
         temp=cp.stdout
         if int(float(temp[0:4]))!=int(float(msg2)):
             # if not set correct, try to set it again
-            msg1="ebusctl write -c b7v z1DayTemp "
+            msg1="ebusctl write -c b7v z2DayTemp "
             cp = subprocess.run([msg1+msg2],shell=True,stdout=subprocess.PIPE)
             time.sleep(15)
         else:
             correctSetting=True
-            print('setting z1DayTemp matches')
-            print(correctSetting)
+            logging.debug('setting z2DayTemp matches')
+            logging.debug(correctSetting)
         cntr1=cntr1+1
 
     correctSetting=False
@@ -66,30 +81,24 @@ with ILock('ebus', timeout=200):
         cntr=0
         busread='ERR'
         while (busread[0:3]=='ERR') and (cntr<numberOfIterationsMax):
-            cp = subprocess.run(["ebusctl read z1NightTemp"],shell=True,stdout=subprocess.PIPE)
+            cp = subprocess.run(["ebusctl read z2NightTemp"],shell=True,stdout=subprocess.PIPE)
             cp_string=cp.stdout.decode('utf-8')
             busread=cp_string[0:5]
-            print(busread)
+            logging.debug("z2NightTemp")
+            logging.debug(busread)
             time.sleep(5)
             cntr=cntr+1
-            print(cntr)
+            logging.debug(cntr)
 
         #check if it is truly set
         temp=cp.stdout
         if int(float(temp[0:4]))!=int(float(msg2)):
             # if not set correct, try to set it again
-            msg1="ebusctl write -c b7v z1NightTemp "
+            msg1="ebusctl write -c b7v z2NightTemp "
             cp = subprocess.run([msg1+msg2],shell=True,stdout=subprocess.PIPE)
             time.sleep(15)
         else:
             correctSetting=True
-            print('setting z1NightTemp matches')
+            logging.debug('setting z2NightTemp matches')
         cntr1=cntr1+1
-    # cp = subprocess.run(["ebusctl read Hc1NightTemp"],shell=True,stdout=subprocess.PIPE)
-    # temp=cp.stdout
-    # if int(float(temp[0:4]))!=int(float(msg2)):
-    #     # if not set correct
-    #     msg1="ebusctl write -c f37 Hc1NightTemp "
-    #     cp = subprocess.run([msg1+msg2],shell=True,stdout=subprocess.PIPE)
-    # else:
-    #     print("setting correct")
+
